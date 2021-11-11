c45ad8f68ff7582b4b41aa9cdef538763ad5a4a4
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2021-11-12 04:23:06 +0600

    Fix bug and use argparse for cmd line args

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import argparse
+from pprint import pprint
 import re
 import sys
-
 from sudoku import Sudoku
 
 
@@ -16,30 +17,32 @@ def convert_input_to_int_list(text):
 
 
 if __name__ == '__main__':
-    # check if a file name is provided
-    n = len(sys.argv)
-    if n < 3:
-        print(f"Usage: {sys.argv[0]} <input file> <output file>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="Solves a valid sudoku puzzle")
+    parser.add_argument('input_file', help="Input text file containing 9x9 grid of spaced separated "
+                                           "integers 0 - 9, where 0 represents an empty cell")
+    parser.add_argument('-o', '--output_file', help="Output text file with the solution of the puzzle. "
+                                                    "If not provided, the output is shown to the console")
+    args = parser.parse_args()
+    input_file_name = args.input_file
+    output_file_name = args.output_file
 
     # open input file or throw exception if not found
     try:
-        in_file = open(sys.argv[1], 'r')
-        in_file.close()
+        input_file_handle = open(input_file_name, 'r')
     except FileNotFoundError:
         print("Input file not found")
         sys.exit(1)
 
-    # open output file or throw exception if not found
-    try:
-        out_file = open(sys.argv[2], 'w')
-        out_file.close()
-    except FileExistsError:
-        print("File already exists")
-        sys.exit(1)
+    if output_file_name:
+        # open output file or throw exception if not found
+        try:
+            output_file_handle = open(output_file_name, 'w')
+        except FileExistsError:
+            print("File already exists")
+            sys.exit(1)
 
     try:
-        grid = convert_input_to_int_list(in_file.readlines())
+        grid = convert_input_to_int_list(input_file_handle.read())
     except ValueError:
         print('Invalid input file')
         sys.exit(1)
@@ -50,4 +53,14 @@ if __name__ == '__main__':
         if not solution:
             print("Solution not possible")
         else:
-            print(solution)
+            if output_file_name:
+                for row in solution:
+                    row = list(map(str, row))
+                    output_file_handle.writelines(' '.join(row))
+                    output_file_handle.writelines('\n')
+            else:
+                print("Solution of the puzzle:")
+                pprint(solution)
+    input_file_handle.close()
+    if output_file_name:
+        output_file_handle.close()
